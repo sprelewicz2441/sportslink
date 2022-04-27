84b389c892ceabc903d4c79e3f4664d2a9adeb11
Scott R Prelewicz <sprelewicz@gmail.com>
2022-04-26 21:20:47 -0400

    Change clue

--- js/Game.js
+++ js/Game.js
@@ -7,12 +7,17 @@ export default class Game {
     this.current_row = 0;
     this.current_col = 0;
     this.currentGuess = '';
-    this.wordOfDay = "thome"
+    this.wordOfDay = "bauer"
   }
 
   handleKeyPress(letter) {
     if(letter == "ENT") {
       this.checkWord();
+      return;
+    }
+
+    if(letter = "DEL") {
+      this.deleteChar();
       return;
     }
     this.game_matix[this.current_row][this.current_col].innerHTML = letter;
