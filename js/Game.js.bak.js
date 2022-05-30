f79f5670002cad375d65538adcdcd95d471fcb98
Scott R Prelewicz <sprelewicz@gmail.com>
2022-05-30 16:26:58 -0400

    Change WOD

--- js/Game.js.bak.js
+++ js/Game.js.bak.js
@@ -9,8 +9,8 @@ export default class Game {
     this.currentGuess = '';
     this.game_status = '';
     this.allGuesses = [];
-    this.wordOfDay = "pysyk";
-    this.game_hint = "Sabres D";
+    this.wordOfDay = "saban";
+    this.game_hint = "60's Bills";
     this.alert_box = document.querySelector("#alerts");
 
     // Setup cache
@@ -25,7 +25,7 @@ export default class Game {
     this.stampkey = "buffastamp";
     this.from_cache_complete = false;  //Flag to see if game was loaded from cache
 
-    let wod_ts = 30;
+    let wod_ts = 35;
 
     let self = this;
     self.lcldb.getItem(self.stampkey, function(err, stamp) {
